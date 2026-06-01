c1b0f119a6024d4f3f6a8b7def19b9c5f060ebc8
maldonadoenzo88-cell <maldonadoenzo88@gmail.com>
2026-06-01 08:20:42 -0300

    NUEVOS

---
+++ TP5_EJ1/src/tp5_ej1/TP5_EJ1.java
@@ -0,0 +1,20 @@
+package tp5_ej1;
+
+public class TP5_EJ1 {
+
+    public static void main(String[] args) {
+        int matriz[][] = {
+{4, 5},
+{3, 2}
+};
+
+for (int fila = 0; fila < 2; fila++) {
+for (int columna = 0; columna < 2; columna++) {
+System.out.print("[" + matriz[fila][columna] + "]");
+}
+System.out.println();
+}
+}
+
+    }
+        
